e9727c07e32301692a6782791ec531f1fbbb90f8
astrajoan <54871344+astrajoan@users.noreply.github.com>
2023-07-13 13:58:06 -0700

    Add installation script for cli tool

---
+++ cli/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [[ $EUID != 0 ]];
+    then echo "Please run as root!"
+    exit 1
+fi
+
+npm pack &>/dev/null
+
+cli_path="pastecat-cli-1.0.0.tgz"
+config_path="../config.js"
+bin_path=$(npm root -g)
+
+npm install -g $cli_path 1>/dev/null
+cp $config_path "${bin_path}/pastecat-cli/node_modules/pastecat-utils/"
+rm $cli_path
+
+echo Successfully installed pastecat-cli 🐿️ 🐖!
